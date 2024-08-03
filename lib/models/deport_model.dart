a768fcc639b7f334e9c1c09440f2622d1a1ff66a
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-08-03 10:08:00 -0500

    deportmodel

---
+++ lib/models/deport_model.dart
@@ -0,0 +1,17 @@
+class DeportModel {
+  String name;
+  bool isFavorite;
+  DeportModel(this.name, this.isFavorite);
+}
+
+List<DeportModel> deportModelList = [
+  DeportModel("Gimnasio", false),
+  DeportModel("Voley", false),
+  DeportModel("Baloncesto", false),
+  DeportModel("Atletismo", false),
+  DeportModel("Natación", false),
+  DeportModel("Kaiac", false),
+  DeportModel("Balonmano", false),
+  DeportModel("Paracaidismo", false),
+  DeportModel("Caminata", false),
+];
