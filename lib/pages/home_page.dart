cf244279ea33bee64f634c811aecb9ce50f76a92
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-08-02 19:56:22 -0500

    flutter lints

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text("Inicio de sesión"),
+        ],
+      ),
+    );
   }
 }
